f5d4bde601c4671088c7b45c16eb2ff430a3eb46
hoangthach1402 <vuhoangthach1402@gmail.com>
2025-04-29 16:51:44 +0700

    remove tb access token

---
+++ database/migrations/2025_04_29_163400_create_api_keys_table.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateApiKeysTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('api_keys', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->string('key', 80)->unique();
+            $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('api_keys');
+    }
+}
